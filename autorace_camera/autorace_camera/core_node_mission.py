Added intersection sign spawn, fixed image projection

autorace_camera/autorace_camera/core_node_mission.py
@@ -42,13 +42,13 @@ class ControlMission(Node):
         with open(green_light_path, 'r') as glm:
             self.green_light_model = glm.read().replace("\n", "")
 
-        # traffic_left_path = model_dir_path + '/traffic_left/model.sdf'
-        # traffic_left_model = open(traffic_left_path, 'r')
-        # self.traffic_left_model = traffic_left_model.read()
+        traffic_left_path = model_dir_path + '/intersection/left.sdf'
+        with open(traffic_left_path, 'r') as tlm:
+            self.traffic_left_model = tlm.read().replace("\n", "")
 
-        # traffic_right_path = model_dir_path + '/traffic_right/model.sdf'
-        # traffic_right_model = open(traffic_right_path, 'r')
-        # self.traffic_right_model = traffic_right_model.read()
+        traffic_right_path = model_dir_path + '/intersection/right.sdf'
+        with open(traffic_right_path, 'r') as trm:
+            self.traffic_right_model = trm.read().replace("\n", "")
 
         # up_bar_path = model_dir_path + '/traffic_bar_up/model.sdf'
         # up_bar_model = open(up_bar_path, 'r')
@@ -148,19 +148,28 @@ class ControlMission(Node):
                     p = subprocess.run(arg)
                     self.traffic_state = 4
 
-            # elif self.traffic_state == 4:
-            #     if abs(self.current_time - time.time()) > 5:  # intersections
-            #         rospy.wait_for_service('gazebo/spawn_sdf_model')
-            #         del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
-            #         intersection_direction = np.random.rand()
+            elif self.traffic_state == 4: # intersections
+                intersection_direction = random.random()
 
-            #         if intersection_direction < 0.5:
-            #             del_model_prox('traffic_right')
+                if intersection_direction < 0.5:
+                    command = ["gz", "service", "-s", "/world/course/create",
+                    "--reqtype", "gz.msgs.EntityFactory",
+                    "--reptype", "gz.msgs.Boolean",
+                    "--timeout", "300",
+                    "--req", f'sdf: "{self.traffic_left_model}"']
 
-            #         else:
-            #             del_model_prox('traffic_left')
+                    p = subprocess.run(command)
 
-            #         self.traffic_state = 5
+                else:
+                    command = ["gz", "service", "-s", "/world/course/create",
+                    "--reqtype", "gz.msgs.EntityFactory",
+                    "--reptype", "gz.msgs.Boolean",
+                    "--timeout", "300",
+                    "--req", f'sdf: "{self.traffic_right_model}"']
+
+                    p = subprocess.run(command)
+
+                self.traffic_state = 5
 
             # elif self.traffic_state == 6:  # bar down.
             #     rospy.wait_for_service('gazebo/spawn_sdf_model')

autorace_camera/autorace_camera/image_projection.py
@@ -37,13 +37,14 @@ class ImageProjection(Node):
             # publishes ground-project image in raw type 
             self.pub_image_projected = self.create_publisher(Image, '/color/image_output', 1)
 
-        # if self.calib == True:
-        #     if self.pub_image_type == "compressed":
-        #         # publishes calibration image in compressed type 
-        #         self.pub_image_calib = self.create_publisher(CompressedImage, '/color/image_calib/compressed', 1)
-        #     elif self.pub_image_type == "raw":
-        #         # publishes calibration image in raw type 
-        #         self.pub_image_calib = self.create_publisher(Image, '/color/image_calib', 1)
+        self.calib = self.get_parameter("is_calibrating").get_parameter_value().bool_value
+        if self.calib == True:
+            if self.pub_image_type == "compressed":
+                # publishes calibration image in compressed type 
+                self.pub_image_calib = self.create_publisher(CompressedImage, '/color/image_calib/compressed', 1)
+            elif self.pub_image_type == "raw":
+                # publishes calibration image in raw type 
+                self.pub_image_calib = self.create_publisher(Image, '/color/image_calib', 1)
 
         self.cvBridge = CvBridge()
 
@@ -52,7 +53,6 @@ class ImageProjection(Node):
         self.top_y = self.get_parameter("top_y").get_parameter_value().integer_value
         self.bottom_x = self.get_parameter("bottom_x").get_parameter_value().integer_value
         self.bottom_y = self.get_parameter("bottom_y").get_parameter_value().integer_value
-        self.calib = self.get_parameter("is_calibrating").get_parameter_value().bool_value
 
         if self.sub_image_type == "compressed":
             # converts compressed image to opencv image
@@ -73,10 +73,10 @@ class ImageProjection(Node):
             cv_image_calib = np.copy(cv_image_original)
 
             # draw lines to help setting homography variables
-            cv_image_calib = cv2.line(cv_image_calib, (160 - top_x, 180 - top_y), (160 + top_x, 180 - top_y), (0, 0, 255), 1)
-            cv_image_calib = cv2.line(cv_image_calib, (160 - bottom_x, 120 + bottom_y), (160 + bottom_x, 120 + bottom_y), (0, 0, 255), 1)
-            cv_image_calib = cv2.line(cv_image_calib, (160 + bottom_x, 120 + bottom_y), (160 + top_x, 180 - top_y), (0, 0, 255), 1)
-            cv_image_calib = cv2.line(cv_image_calib, (160 - bottom_x, 120 + bottom_y), (160 - top_x, 180 - top_y), (0, 0, 255), 1)
+            cv_image_calib = cv2.line(cv_image_calib, (424 - top_x, 240 - top_y), (424 + top_x, 240 - top_y), (0, 0, 255), 1)
+            cv_image_calib = cv2.line(cv_image_calib, (424 - bottom_x, 240 + bottom_y), (424 + bottom_x, 240 + bottom_y), (0, 0, 255), 1)
+            cv_image_calib = cv2.line(cv_image_calib, (424 + bottom_x, 240 + bottom_y), (424 + top_x, 240 - top_y), (0, 0, 255), 1)
+            cv_image_calib = cv2.line(cv_image_calib, (424 - bottom_x, 240 + bottom_y), (424 - top_x, 240 - top_y), (0, 0, 255), 1)
 
             if self.pub_image_type == "compressed":
                 # publishes calibration image in compressed type
@@ -91,23 +91,23 @@ class ImageProjection(Node):
 
         ## homography transform process
         # selecting 4 points from the original image
-        pts_src = np.array([[160 - top_x, 180 - top_y], [160 + top_x, 180 - top_y], [160 + bottom_x, 120 + bottom_y], [160 - bottom_x, 120 + bottom_y]])
+        pts_src = np.array([[424 - top_x, 240 - top_y], [424 + top_x, 240 - top_y], [424 + bottom_x, 240 + bottom_y], [424 - bottom_x, 240 + bottom_y]])
 
         # selecting 4 points from image that will be transformed
-        pts_dst = np.array([[200, 0], [800, 0], [800, 600], [200, 600]])
+        pts_dst = np.array([[148, 0], [600, 0], [600, 480], [148, 480]])
 
         # finding homography matrix
         h, status = cv2.findHomography(pts_src, pts_dst)
 
         # homography process
-        cv_image_homography = cv2.warpPerspective(cv_image_original, h, (1000, 600))
+        cv_image_homography = cv2.warpPerspective(cv_image_original, h, (848, 480))
 
         # fill the empty space with black triangles on left and right side of bottom
-        triangle1 = np.array([[0, 599], [0, 340], [200, 599]], np.int32)
-        triangle2 = np.array([[999, 599], [999, 340], [799, 599]], np.int32)
-        black = (0, 0, 0)
-        white = (255, 255, 255)
-        cv_image_homography = cv2.fillPoly(cv_image_homography, [triangle1, triangle2], black)
+        # triangle1 = np.array([[0, 479], [0, 260], [148, 479]], np.int32)
+        # triangle2 = np.array([[847, 479], [847, 260], [647, 479]], np.int32)
+        # black = (0, 0, 0)
+        # white = (255, 255, 255)
+        # cv_image_homography = cv2.fillPoly(cv_image_homography, [triangle1, triangle2], black)
 
         if self.pub_image_type == "compressed":
             # publishes ground-project image in compressed type